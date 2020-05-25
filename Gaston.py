9aca4188db5fe1f4be63003c9e04305a81044043
rayhaner <65264006+rayhaner@users.noreply.github.com>
2020-05-25 23:57:51 +0800

    Initial commit

---
+++ Gaston.py
@@ -0,0 +1,57 @@
+from kivy.properties import StringProperty, ObjectProperty
+from kivymd.app import MDApp
+from kivy.uix.boxlayout import BoxLayout
+from kivy.clock import Clock
+from functools import partial
+
+import trackhandling
+
+
+class Gaston(BoxLayout):
+    description_text = StringProperty("This program can generate an edited version of the Gaston song from the 1991 "
+                                      "Disney film Beauty and the Beast, randomly or based on user's selection.")
+
+    default_text = StringProperty("When I was a LAD I ate FOUR DOZEN EGGS every morning to help me get LARGE,\n "
+                                  "But now that I'm GROWN I eat FIVE DOZEN EGGS so I'm roughly the size of a BARGE!")
+
+    preview = ObjectProperty(None)
+
+    def play_callback(self, track, *args):
+        trackhandling.play_track(track)
+
+    def change_choice(self, num, obj):
+        app = MDApp.get_running_app()
+        app.choices[num] = trackhandling.string_dict_rev[obj.text]
+
+    def get_preview(self):
+        app = MDApp.get_running_app()
+        self.preview.text = str(trackhandling.to_string(*app.choices))
+
+    def generate(self):
+        app = MDApp.get_running_app()
+        track, text = trackhandling.make_track(*app.choices)
+        self.preview.text = str(text)
+        Clock.schedule_once(partial(self.play_callback, track))
+
+    def randomize(self):
+        track, text = trackhandling.randomize(same_num=True)
+        self.preview.text = str(text)
+        Clock.schedule_once(partial(self.play_callback, track))
+
+    def same_word_lyrics(self):
+        track, text = trackhandling.same_word()
+        self.preview.text = str(text)
+        Clock.schedule_once(partial(self.play_callback, track))
+
+
+class GastonApp(MDApp):
+    choices = trackhandling.original
+    noun_values = list(trackhandling.string_dict.values())[:5]
+    number_values = list(trackhandling.string_dict.values())[-2:]
+
+    def build(self):
+        return Gaston()
+
+
+if __name__ == '__main__':
+    GastonApp().run()
---
+++ trackhandling.py
@@ -0,0 +1,78 @@
+from pydub import AudioSegment
+from pydub.playback import play
+from random import choice
+
+file_list = ['gaston_full', 'when_i_was_a', 'lad', 'i_ate', 'four_dozen', 'eggs1', 'every_morning', 'large',
+             'and_now', 'grown', 'i_eat', 'five_dozen', 'eggs2', 'roughly', 'barge']
+nouns = ['lad', 'eggs1', 'eggs2', 'barge', 'grown']
+numbers = ['four_dozen', 'five_dozen']
+
+original = ['lad', 'four_dozen', 'eggs1', 'large', 'grown', 'five_dozen', 'eggs2', 'barge']
+
+string_dict = {
+    'grown': 'GROWN',
+    'lad': 'LAD',
+    'large': 'LARGE',
+    'barge': 'BARGE',
+    'eggs1': 'EGGS',
+    'eggs2': 'EGGS',
+    'five_dozen': 'FIVE DOZEN',
+    'four_dozen': 'FOUR DOZEN'
+}
+
+string_dict_rev = {value: key for key, value in string_dict.items() if key != 'eggs2'}
+
+
+def import_tracks():
+    def from_string(s):
+        return AudioSegment.from_mp3('samples/' + s + '.mp3')
+
+    t_list = {}
+    for filename in file_list:
+        t_list[filename] = from_string(filename)
+
+    return t_list
+
+
+def to_string(*args):
+    return 'When I was a ' + string_dict[args[0]] + ' I ate ' + string_dict[args[1]] + ' ' + string_dict[args[2]] + \
+           ' every morning to help me get ' + string_dict[args[3]] + "!\nBut now that I'm " + string_dict[args[4]] + \
+           ' I eat ' + string_dict[args[5]] + ' ' + string_dict[args[6]] + " so I'm roughly the size of a " + \
+           string_dict[args[7]] + '!'
+
+
+t = import_tracks()
+
+
+def make_track(*input):
+    slots = list(input)
+    if slots[2] == 'eggs2':
+        slots[2] = 'eggs1'
+    if slots[6] == 'eggs1':
+        slots[6] = 'eggs2'
+    out_str = to_string(*slots)
+    return t['when_i_was_a'] + t[slots[0]] + t['i_ate'] + t[slots[1]] + t[slots[2]] + \
+           t['every_morning'] + t[slots[3]] + t['and_now'] + t[slots[4]] + t['i_eat'] + t[slots[5]] + \
+           t[slots[6]] + t['roughly'] + t[slots[7]], out_str
+
+
+def play_track(track):
+    play(track)
+
+
+def randomize(same_num=False):
+    slots = [choice(nouns), choice(numbers), choice(nouns[:4]), choice(nouns), choice(nouns), choice(numbers),
+             choice(nouns[:4]), choice(nouns)]
+
+    if same_num:
+        slots[1] = 'four_dozen'
+        slots[5] = 'five_dozen'
+
+    return make_track(*slots)
+
+
+def same_word(word=None):
+    if word is None:
+        word = choice(nouns)
+
+    return make_track(word, 'four_dozen', word, word, word, 'five_dozen', word, word)
